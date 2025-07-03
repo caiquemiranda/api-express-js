e168d9ffd170ee15d164a8854bff7ce62f595fc6
caiquemiranda <caiquemirandas@gmail.com>
2025-07-03 20:30:56 -0300

    servidor funcionando

--- fundamentos/src/index.ts
+++ fundamentos/src/index.ts
@@ -3,6 +3,10 @@ import express from "express"
 const app = express()
 const porta = 8080
 
+app.use("/", (req, res)=>{
+    res.send("<h1>Olá App</h1>")
+})
+
 app.listen(porta, ()=>{
     console.log("Servidor Rodando")
 })
